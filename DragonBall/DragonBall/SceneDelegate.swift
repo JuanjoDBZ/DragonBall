97165292adf3feef61ee2b535d73d3718c06ab88
JuanjoDBZ <juan.esquivel@isita.com>
2021-08-20 19:13:49 -0500

    -Prueba

--- DragonBall/DragonBall/SceneDelegate.swift
+++ DragonBall/DragonBall/SceneDelegate.swift
@@ -21,6 +21,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                 window?.windowScene = windowScene
                 let login = DBLandingScreenRouter()
         let navigationController = UINavigationController(rootViewController: login.view)
+        
                 navigationController.modalPresentationStyle = .fullScreen
                 window?.rootViewController = navigationController
                 window?.makeKeyAndVisible()
